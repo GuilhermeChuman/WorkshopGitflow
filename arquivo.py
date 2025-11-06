abc97056a873c6b631a867b9e78fd20d7c8c9765
Guilherme Chuman Troncon <guilhermectroncon@gmail.com>
2025-11-06 16:20:58 -0300

    Update arquivo.py

--- arquivo.py
+++ arquivo.py
@@ -1,12 +1,10 @@
 import subprocess
 
 def main():
-
     entrada = input("Entre com uma feature: ")
     feature(entrada)
 
 def feature(featureIndex):
-    
     subprocess.run(["python", './WorkshopGitflow/'+featureIndex+".py"])
 
 main()
